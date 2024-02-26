7c0e4e8d534ef410d8692335452e1ca9e3424d31
agrmet <agron.metaj.3321@student.uu.se>
2024-02-26 15:23:29 +0100

    Initialize database with data from example CSV

--- BlazingParon/Data/SeedData.cs
+++ BlazingParon/Data/SeedData.cs
@@ -12,7 +12,28 @@ namespace BlazingParon.Data
                 new Product { Name = "Päronklocka", Price = 11000 }
 
             };
+
+            var warehouses = new Warehouse[]
+            {
+                new Warehouse { Name = "Cupertino" },
+                new Warehouse { Name = "Norrköping" },
+                new Warehouse { Name = "Frankfurt" }
+            };
+            var inventorycounts = new InventoryCount[]
+            {
+                new InventoryCount { ProductId = 1, WarehouseId = 1, Quantity = 170000 },
+                new InventoryCount { ProductId = 2, WarehouseId = 1, Quantity = 41500 },
+                new InventoryCount { ProductId = 3, WarehouseId = 1, Quantity = 90000 },
+                new InventoryCount { ProductId = 1, WarehouseId = 2, Quantity = 101700 },
+                new InventoryCount { ProductId = 2, WarehouseId = 2, Quantity = 72400 },
+                new InventoryCount { ProductId = 3, WarehouseId = 2, Quantity = 25000 },
+                new InventoryCount { ProductId = 1, WarehouseId = 3, Quantity = 55000 },
+                new InventoryCount { ProductId = 2, WarehouseId = 3, Quantity = 104300 },
+                new InventoryCount { ProductId = 3, WarehouseId = 3, Quantity = 38000 }
+            };
             db.Products.AddRange(products);
+            db.Warehouses.AddRange(warehouses);
+            db.InventoryCounts.AddRange(inventorycounts);
             db.SaveChanges();
         }
     }
